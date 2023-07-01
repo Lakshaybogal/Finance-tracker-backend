7f0323d2f5c52f720c0501e1c822e12c9d95ff25
Lakshay Rajput <rajputlakshay553@gmail.com>
2023-07-01 08:39:55 +0530

    ready to deploy

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
+
 const app = express();
 const routes = require('./routes/routes');
+
 app.use(cors());
 app.use(express.json());
-app.use('/',routes)
-mongoose.connect(process.env.DB_KEY).then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log('listening on PORT ' + process.env.PORT);
-        console.log('Conected to db');
-    })
-}
-)
+app.use('/', routes);
 
+mongoose
+    .connect(process.env.DB_KEY, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log('Listening on PORT ' + process.env.PORT);
+            console.log('Connected to the database');
+        });
+    })
+    .catch((error) => {
+        console.error('Error connecting to the database:', error);
+    });
